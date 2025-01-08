e1b3835a105ee2d2b0fc64679c7f794590c2fea2
Bond <1404933162@qq.com>
2025-01-08 10:40:56 +0800

    update Errorboundary component

--- shopproject-frontend/src/components/ErrorBoundary.js
+++ shopproject-frontend/src/components/ErrorBoundary.js
@@ -1,32 +1,24 @@
 import React, { Component } from 'react'; // 导入React和Component类
+import { message } from 'antd';
 
 // 创建一个错误边界组件
 class ErrorBoundary extends Component {
-    // 定义组件的状态
-    state = {
-        hasError: false, // 是否捕获到错误
-        error: null, // 捕获的错误信息
-        errorInfo: null, // 错误的附加信息
-    };
+    state = { hasError: false };
 
-    // 组件生命周期方法，捕获错误并更新状态
     componentDidCatch(error, errorInfo) {
-        this.setState({
-            hasError: true, // 设置已捕获错误的状态为true
-            error, // 保存错误信息
-            errorInfo, // 保存错误的附加信息
-        });
+        this.setState({ hasError: true });
+        message.error('Something went wrong. Please try again later.');
+        console.error('Error:', error, 'Error Info:', errorInfo); // 记录错误信息
+        
     }
 
-    // 渲染方法
     render() {
-        // 如果捕获到错误，显示错误提示
         if (this.state.hasError) {
-            return <h1>Something went wrong.</h1>; // 可以修改为自定义的错误信息展示
+            return <h1>Something went wrong.</h1>;
         }
-        // 否则正常渲染子组件
-        return this.props.children; // 渲染子组件
+        return this.props.children;
     }
 }
+
 
 export default ErrorBoundary; // 导出错误边界组件
